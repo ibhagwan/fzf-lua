cee65b096c30e7296e253b0d321c6afd3117bdad
bhagwan <bhagwan@disroot.org>
2022-10-16 14:59:10 -0700

    fix(diagnostics): error when diag sign is nil (closes #543)

--- lua/fzf-lua/providers/diagnostic.lua
+++ lua/fzf-lua/providers/diagnostic.lua
@@ -68,7 +68,8 @@ M.diagnostics = function(opts)
     -- can be empty when config set to (#480):
     -- vim.diagnostic.config({ signs = false })
     if vim.tbl_isempty(sign_def) then sign_def = nil end
-    opts.__signs[v.severity].text = sign_def and vim.trim(sign_def[1].text) or v.default
+    opts.__signs[v.severity].text = sign_def and sign_def[1].text and
+      vim.trim(sign_def[1].text) or v.default
     opts.__signs[v.severity].texthl = sign_def and sign_def[1].texthl or nil
     if opts.signs and opts.signs[k] and opts.signs[k].text then
       opts.__signs[v.severity].text = opts.signs[k].text
