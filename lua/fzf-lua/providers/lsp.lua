c0427c71a5fca05b31f010ee5d2996aa567259b2
Xianzhe Liang <liangxianzhe@gmail.com>
2023-03-11 13:14:38 -0800

    feat(symbols): better symbol kind icons.
    Addresses a few issues:
    
    1. The existing method of using CmpItemKind is not right.
    
    CmpItemKind it was designed for completion, not for symbols. It includes
    things like Snippet, but doesn't cover all the symbols defined in LSP
    spec, such as String, Array, Object.
    
    Additionally, fetching icons from the protocol object is not the best
    practice in my mind. The default protocol object is just symbol name and
    id. There are a few plugins using this practice, but many others (cmp,
    aerial, symbol-outline) asks user to set icons directly.
    
    Providing some default icon is something debatable. We can also do not
    provide any default icons, and rely on user to set them manually.
    
    2. The highlight groups are changed.
    
    `TSFunction` is already removed from Treesitter implementation. The
    default one now is `@function`.
    
    3. A minor issue when substitute `[Kind]`.
    
    It should only substitute the first match, otherwise, if the symbol it
    self includes brackets (e.g. `[-nth=foo]`), it will be substitute too.

--- lua/fzf-lua/providers/lsp.lua
+++ lua/fzf-lua/providers/lsp.lua
@@ -129,7 +129,7 @@ local function symbols_to_items(symbols, bufnr, child_prefix)
           lnum = range.start.line + 1,
           col = range.start.character + 1,
           kind = kind,
-          text = prefix ..  '[' .. kind .. '] ' .. symbol.name,
+          text = prefix .. "[" .. kind .. "] " .. symbol.name,
         })
       elseif symbol.selectionRange then -- DocumentSymbole type
         table.insert(_items, {
@@ -138,7 +138,7 @@ local function symbols_to_items(symbols, bufnr, child_prefix)
           lnum = symbol.selectionRange.start.line + 1,
           col = symbol.selectionRange.start.character + 1,
           kind = kind,
-          text = prefix .. '[' .. kind .. '] ' .. symbol.name,
+          text = prefix .. "[" .. kind .. "] " .. symbol.name,
         })
         if symbol.children then
           for _, v in ipairs(_symbols_to_items(symbol.children, _items, _bufnr, prefix .. child_prefix)) do
@@ -169,7 +169,7 @@ local function symbol_handler(opts, cb, _, result, _, _)
       if M._sym2style then
         local kind = entry.text:match("%[(.-)%]")
         if kind and M._sym2style[kind] then
-          entry.text = entry.text:gsub("%[.-%]", M._sym2style[kind])
+          entry.text = entry.text:gsub("%[.-%]", M._sym2style[kind], 1)
         end
       end
       entry = make_entry.lcol(entry, opts)
@@ -464,34 +464,25 @@ local function gen_sym2style_map(opts)
   assert(opts.symbol_style ~= nil)
   M._sym2style = {}
   local colormap = vim.api.nvim_get_color_map()
-  for k, v in pairs(vim.lsp.protocol.CompletionItemKind) do
-    if type(k) == "string" then
-      local icon = vim.lsp.protocol.CompletionItemKind[v]
-      -- style==1: "<icon> <kind>"
-      -- style==2: "<icon>"
-      -- style==3: "<kind>"
-      local s = nil
-      if opts.symbol_style == 1 then
-        -- if icons weren't set by the user
-        -- icon will match the kind
-        if icon ~= k then
-          s = ("%s %s"):format(icon, k)
-        else
-          s = k
-        end
-      elseif opts.symbol_style == 2 then
-        s = icon
-      elseif opts.symbol_style == 3 then
-        s = k
-      end
-      if s and opts.symbol_hl_prefix then
-        M._sym2style[k] = utils.ansi_from_hl(opts.symbol_hl_prefix .. k, s, colormap)
-      elseif s then
-        M._sym2style[k] = s
-      else
-        -- can get here when only 'opts.symbol_fmt' was set
-        M._sym2style[k] = k
-      end
+  for kind, icon in pairs(opts.symbol_icons) do
+    -- style==1: "<icon> <kind>"
+    -- style==2: "<icon>"
+    -- style==3: "<kind>"
+    local s = nil
+    if opts.symbol_style == 1 and config._has_devicons then
+      s = ("%s %s"):format(icon, kind)
+    elseif opts.symbol_style == 2 and config._has_devicons then
+      s = icon
+    elseif opts.symbol_style == 3 then
+      s = kind
+    end
+    if s and opts.symbol_hl then
+      M._sym2style[kind] = utils.ansi_from_hl(opts.symbol_hl(kind), s, colormap)
+    elseif s then
+      M._sym2style[kind] = s
+    else
+      -- can get here when only 'opts.symbol_fmt' was set
+      M._sym2style[kind] = kind
     end
   end
   if type(opts.symbol_fmt) == "function" then
