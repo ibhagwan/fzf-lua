e1347042e4e26bc9842251d5bcc820c94acbd1cb
Yao Weijie <yaowj.it@outlook.com>
2023-12-18 13:27:36 +0800

    fix(bat): adjust bat default cmd batcat/bat(#800)

--- lua/fzf-lua/defaults.lua
+++ lua/fzf-lua/defaults.lua
@@ -141,17 +141,17 @@ M.defaults = {
       _ctor = previewers.fzf.cmd,
     },
     bat = {
-      cmd   = "bat",
+      cmd   = vim.fn.executable("batcat") == 1 and "batcat" or "bat",
       args  = "--color=always --style=numbers,changes",
       _ctor = previewers.fzf.bat_async,
     },
     bat_native = {
-      cmd   = "bat",
+      cmd   = vim.fn.executable("batcat") == 1 and "batcat" or "bat",
       args  = "--color=always --style=numbers,changes",
       _ctor = previewers.fzf.bat,
     },
     bat_async = {
-      cmd   = "bat",
+      cmd   = vim.fn.executable("batcat") == 1 and "batcat" or "bat",
       args  = "--color=always --style=numbers,changes",
       _ctor = previewers.fzf.bat_async,
     },
