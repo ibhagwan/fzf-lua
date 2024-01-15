953672442741219ca825f906f85ccec83143d66b
lbrayner <brayner.l@gmail.com>
2024-01-15 12:38:13 -0300

    Encode tab_cwd in base64 (*NIX file names are permissive)

--- lua/fzf-lua/providers/buffers.lua
+++ lua/fzf-lua/providers/buffers.lua
@@ -384,8 +384,9 @@ M.tabs = function(opts)
           function(s) return s end,
           utils.ansi_codes[opts.hls.tab_marker])
 
+        local tab_cwd_tilde_base64 = vim.base64.encode(tab_cwd_tilde)
         if not opts.current_tab_only then
-          cb(string.format("%d)%s:%s%s\t%s", t, tab_cwd_tilde, utils.nbsp,
+          cb(string.format("%d)%s:%s%s\t%s", t, tab_cwd_tilde_base64, utils.nbsp,
             fn_title_hl(title),
             (t == core.CTX().tabnr) and fn_marker_hl(marker) or ""))
         end
@@ -397,7 +398,7 @@ M.tabs = function(opts)
 
         opts.sort_lastused = false
         opts._prefix = string.format("%d)%s:%s%s%s",
-          t, tab_cwd_tilde, utils.nbsp, utils.nbsp, utils.nbsp)
+          t, tab_cwd_tilde_base64, utils.nbsp, utils.nbsp, utils.nbsp)
         local tabh = vim.api.nvim_list_tabpages()[t]
         local buffers = populate_buffer_entries(opts, bufnrs_flat, tabh)
         for _, bufinfo in pairs(buffers) do
