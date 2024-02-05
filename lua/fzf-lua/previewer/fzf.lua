ibhagwan/fzf-lua

commit 76ddb46f4a5d799555e19761735ef4842162d3cd

fix: do not nullify `--delimiter` with fzf native previewer

lua/fzf-lua/previewer/fzf.lua
@@ -42,7 +42,6 @@ function Previewer.base:preview_offset()
 end
 
 function Previewer.base:fzf_delimiter()
-  if not self.opts.line_field_index then return end
   -- set delimiter to ':'
   -- entry format is 'file:line:col: text'
   local delim = self.opts.fzf_opts and self.opts.fzf_opts["--delimiter"]

commit ba164376aaaabef82abbe534ef213851c959f0bd

fix: help_native previewer (fixes #1029, caused by 76ddb46)

lua/fzf-lua/previewer/fzf.lua
@@ -49,7 +49,7 @@ function Previewer.base:fzf_delimiter()
     delim = "[:]"
   elseif not delim:match(":") then
     if delim:match("%[.*%]") then
-      delim = delim:match("(%[.*)%]") .. ":]"
+      delim = delim:gsub("%]", ":]")
     else
       -- remove surrounding quotes
       delim = delim:match("^'?(.*)'$?") or delim