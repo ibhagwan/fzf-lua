97b455afe4a6c4d5a7f5c7b2dcab35a86b537872
bhagwan <bhagwan@disroot.org>
2024-03-16 17:05:48 -0700

    fix(grep_visual): instant exit post e8b2093

--- lua/fzf-lua/utils.lua
+++ lua/fzf-lua/utils.lua
@@ -636,10 +636,11 @@ function M.get_visual_selection()
       -- visual line doesn't provide columns
       cscol, cecol = 0, 999
     end
+    -- NOTE: not required since commit: e8b2093
     -- exit visual mode
-    vim.api.nvim_feedkeys(
-      vim.api.nvim_replace_termcodes("<Esc>",
-        true, false, true), "n", true)
+    -- vim.api.nvim_feedkeys(
+    --   vim.api.nvim_replace_termcodes("<Esc>",
+    --     true, false, true), "n", true)
   else
     -- otherwise, use the last known visual position
     _, csrow, cscol, _ = unpack(vim.fn.getpos("'<"))
