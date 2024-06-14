80e4157682214ab839c3192ec0982fa9c5f79ea4
Jan Stocker <Jan.Stocker@cosmoconsult.com>
2024-06-14 17:23:00 +0200

    New function utils.create_user_command_callback

--- lua/fzf-lua/profiles/fzf-vim.lua
+++ lua/fzf-lua/profiles/fzf-vim.lua
@@ -1,5 +1,6 @@
 local fzf_lua = require("fzf-lua")
 local actions = fzf_lua.actions
+local utils = fzf_lua.utils
 
 local function cmd_exists(cmd)
   local ret = vim.fn.exists(":" .. cmd)
@@ -9,61 +10,29 @@ local function cmd_exists(cmd)
 end
 
 local function setup_commands(no_override, prefix)
-  local cb_create = function(provider, arg, altmap)
-    local function fzflua_opts(o)
-      local ret = {}
-      -- fzf.vim's bang version of the commands opens fullscreen
-      if o.bang then ret.winopts = { fullscreen = true } end
-      return ret
-    end
-    return function(o)
-      local prov = provider
-      local opts = fzflua_opts(o) -- setup bang!
-      if type(o.fargs[1]) == "string" then
-        local farg = o.fargs[1]
-        for c, p in pairs(altmap or {}) do
-          -- fzf.vim hijacks the first character of the arg
-          -- to setup special commands postfixed with `?:/`
-          -- "GFiles?", "History:" and "History/"
-          if farg:sub(1, 1) == c then
-            prov = p
-            -- we still allow using args with alt
-            -- providers by removing the "?:/" prefix
-            farg = #farg > 1 and vim.trim(farg:sub(2))
-            break
-          end
-        end
-        if arg and farg and #farg > 0 then
-          opts[arg] = vim.trim(farg)
-        end
-      end
-      fzf_lua[prov](opts)
-    end
-  end
-
   local cmds = {
-    ["Files"] = cb_create("files", "cwd"),
-    ["GFiles"] = cb_create("git_files", "cwd", { ["?"] = "git_status" }),
-    ["Buffers"] = cb_create("buffers"),
-    ["Colors"] = cb_create("colorschemes"),
-    ["Rg"] = cb_create("grep", "search"),
-    ["RG"] = cb_create("live_grep", "search"),
-    ["Lines"] = cb_create("lines", "query"),
-    ["BLines"] = cb_create("blines", "query"),
-    ["Tags"] = cb_create("tags", "query"),
-    ["BTags"] = cb_create("btags", "query"),
-    ["Changes"] = cb_create("changes"),
-    ["Marks"] = cb_create("marks"),
-    ["Jumps"] = cb_create("jumps"),
-    ["History"] = cb_create("oldfiles", "query", {
+    ["Files"] = utils.create_user_command_callback("files", "cwd"),
+    ["GFiles"] = utils.create_user_command_callback("git_files", "cwd", { ["?"] = "git_status" }),
+    ["Buffers"] = utils.create_user_command_callback("buffers"),
+    ["Colors"] = utils.create_user_command_callback("colorschemes"),
+    ["Rg"] = utils.create_user_command_callback("grep", "search"),
+    ["RG"] = utils.create_user_command_callback("live_grep", "search"),
+    ["Lines"] = utils.create_user_command_callback("lines", "query"),
+    ["BLines"] = utils.create_user_command_callback("blines", "query"),
+    ["Tags"] = utils.create_user_command_callback("tags", "query"),
+    ["BTags"] = utils.create_user_command_callback("btags", "query"),
+    ["Changes"] = utils.create_user_command_callback("changes"),
+    ["Marks"] = utils.create_user_command_callback("marks"),
+    ["Jumps"] = utils.create_user_command_callback("jumps"),
+    ["History"] = utils.create_user_command_callback("oldfiles", "query", {
       [":"] = "command_history",
       ["/"] = "search_history",
     }),
-    ["Commits"] = cb_create("git_commits", "query"),
-    ["BCommits"] = cb_create("git_bcommits", "query"),
-    ["Maps"] = cb_create("keymaps", "query"),
-    ["Helptags"] = cb_create("help_tags", "query"),
-    ["Filetypes"] = cb_create("filetypes", "query"),
+    ["Commits"] = utils.create_user_command_callback("git_commits", "query"),
+    ["BCommits"] = utils.create_user_command_callback("git_bcommits", "query"),
+    ["Maps"] = utils.create_user_command_callback("keymaps", "query"),
+    ["Helptags"] = utils.create_user_command_callback("help_tags", "query"),
+    ["Filetypes"] = utils.create_user_command_callback("filetypes", "query"),
   }
 
   for cmd, cb in pairs(cmds) do
