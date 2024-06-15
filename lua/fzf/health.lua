44b111e28590b21d5adffd521faa69b19325554e
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-06-15 08:35:32 +0200

    feat: better health

--- lua/fzf/health.lua
+++ lua/fzf/health.lua
@@ -9,37 +9,75 @@ function M.check()
   local utils = require("fzf-lua.utils")
   local config = require("fzf-lua.config")
 
-  local tools = { "fzf", "sk", "rg", "fd", "bat", "delta", "git" }
-  for _, tool in ipairs(tools) do
+  local function have(tool)
     if vim.fn.executable(tool) == 0 then
       warn("'" .. tool .. "' not found")
     else
-      ok("'" .. tool .. "' found")
+      local version = vim.fn.system(tool .. " --version") or ""
+      version = vim.trim(vim.split(version, "\n")[1])
+      ok("'" .. tool .. "' `" .. version .. "`")
+      return true
     end
   end
 
+  start("fzf-lua [required]")
+  if not have("fzf") and not have("sk") then
+    error("`fzf` or `skim` is required")
+  end
   if vim.fn.executable("fzf") == 1 then
     local version = utils.fzf_version()
-    if version >= 0.53 then
-      ok("'fzf' >= 0.53. Your version: '" .. version .. "'")
-    else
-      warn("'fzf' >= 0.53 is recommended. Your version: '" .. version .. "'")
+    if version < 0.53 then
+      warn("'fzf' `>= 0.53` is recommended.")
     end
   end
 
-  if vim.fn.executable("fzf") == 0 and vim.fn.executable("sk") == 0 then
-    error("fzf or skim is required")
+  start("fzf-lua [optional]")
+  if pcall(require, "nvim-web-devicons") then
+    ok("`nvim-web-devicons` found")
+  else
+    warn("`nvim-web-devicons` not found")
+  end
+  for _, tool in ipairs({ "rg", "fd", "bat", "delta", "git" }) do
+    have(tool)
   end
 
-  if vim.env.FZF_DEFAULT_OPTS == nil then
-    ok("'FZF_DEFAULT_OPTS' is not set")
-  else
-    local lines = vim.split(vim.env.FZF_DEFAULT_OPTS, "\n")
-    lines = vim.tbl_map(function(line)
-      return vim.trim(line)
-    end, lines)
-    warn("'FZF_DEFAULT_OPTS' is set to:\n" .. table.concat(lines, "\n"))
+  start("fzf-lua [optional:media]")
+  for _, tool in ipairs({ "chafa", "viu", "ueberzugpp" }) do
+    have(tool)
   end
+
+  start("fzf-lua [env]")
+  if vim.env.FZF_DEFAULT_OPTS == nil then
+    ok("`FZF_DEFAULT_OPTS` is not set")
+  else
+    ok("`$FZF_DEFAULT_OPTS` is set to:\n" .. M.format(vim.env.FZF_DEFAULT_OPTS))
+  end
+  if vim.env.FZF_DEFAULT_OPTS_FILE == nil then
+    ok("`FZF_DEFAULT_OPTS_FILE` is not set")
+  else
+    ok("`FZF_DEFAULT_OPTS_FILE` is set to `" .. vim.env.FZF_DEFAULT_OPTS_FILE .. "`")
+  end
+end
+
+---@param str string
+function M.format(str)
+  str = str:gsub("%s+", " ")
+  local options = vim.split(vim.trim(str), " -", { plain = true })
+  local lines = {}
+  for o, opt in ipairs(options) do
+    opt = o == 1 and opt or ("-" .. opt)
+    opt = vim.trim(opt)
+    opt = opt .. string.rep(" ", math.ceil(#opt / 30) * 30 - #opt)
+    if #lines == 0 or #lines[#lines] > 80 then
+      table.insert(lines, opt)
+    else
+      lines[#lines] = lines[#lines] .. "" .. opt
+    end
+  end
+  lines = vim.tbl_map(function(line)
+    return vim.trim(line)
+  end, lines)
+  return table.concat(lines, "\n")
 end
 
 return M
