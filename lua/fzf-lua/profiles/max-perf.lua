1e03541de4d8a169defe83bb4d7abfba450c63a1
bhagwan <bhagwan@disroot.org>
2024-10-06 16:58:30 -0700

    feat(profiles): added `--ansi` to max-perf profile (#1482)

--- lua/fzf-lua/profiles/max-perf.lua
+++ lua/fzf-lua/profiles/max-perf.lua
@@ -3,12 +3,17 @@ return {
   winopts = { preview = { default = "bat" } },
   manpages = { previewer = "man_native" },
   helptags = { previewer = "help_native" },
+  defaults = { git_icons = false, file_icons = false },
   lsp = { code_actions = { previewer = "codeaction_native" } },
   tags = { previewer = "bat" },
   btags = { previewer = "bat" },
   files = { fzf_opts = { ["--ansi"] = false } },
-  defaults = {
-    git_icons = false,
-    file_icons = false,
+  grep = {
+    fzf_opts  = { ["--ansi"] = false },
+    grep_opts = require("fzf-lua.utils").is_darwin()
+        and "--color=never --binary-files=without-match --line-number --recursive --extended-regexp -e"
+        or "--color=never --binary-files=without-match --line-number --recursive --perl-regexp -e",
+    rg_opts   =
+    " --color=never --column --line-number --no-heading --smart-case --max-columns=4096 -e",
   },
 }
