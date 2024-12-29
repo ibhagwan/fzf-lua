ibhagwan/fzf-lua

commit d99996b5838a30f4725e584bac5e8a0194e5a2a6

fix: `utils.strsplit` empty space between delim (#1650)

lua/fzf-lua/actions.lua
@@ -135,7 +135,7 @@ M.vimcmd_entry = function(_vimcmd, selected, opts, pcall_vimcmd)
   for i, sel in ipairs(selected) do
     (function()
       -- Lua 5.1 goto compatiblity hack (function wrap)
-      local entry = path.entry_to_file(sel, opts, opts._uri)
+      local entry = path.entry_to_file(sel, opts)
       -- "<none>" could be set by `autocmds`
       if entry.path == "<none>" then return end
       local fullpath = entry.bufname or entry.uri and entry.uri:match("^%a+://(.*)") or entry.path

lua/fzf-lua/path.lua
@@ -408,9 +408,8 @@ function M.entry_to_location(entry, opts)
   }
 end
 
-function M.entry_to_file(entry, opts, force_uri)
+function M.entry_to_file(entry, opts)
   opts = opts or {}
-  local cwd = opts.cwd
   if opts._fmt then
     if type(opts._fmt._from) == "function" then
       entry = opts._fmt._from(entry, opts)
@@ -419,33 +418,32 @@ function M.entry_to_file(entry, opts, force_uri)
       entry = opts._fmt.from(entry, opts)
     end
   end
-  -- Remove ansi coloring and prefixed icons
+  -- Remove ANSI coloring and prefixed icons
   entry = utils.strip_ansi_coloring(entry)
   local stripped, idx = stripBeforeLastOccurrenceOf(entry, utils.nbsp)
+  -- Convert "~" to "$HOME"
   stripped = M.tilde_to_HOME(stripped)
+  -- Prepend cwd unless entry is already a URI (e.g. nvim-jdtls "jdt://...")
   local isURI = stripped:match("^%a+://")
-  -- Prepend cwd before constructing the URI (#341)
-  if cwd and #cwd > 0 and not isURI and not M.is_absolute(stripped) then
-    stripped = M.join({ cwd, stripped })
+  if opts.cwd and #opts.cwd > 0 and not isURI and not M.is_absolute(stripped) then
+    stripped = M.join({ opts.cwd, stripped })
   end
-  -- #336: force LSP jumps using 'vim.lsp.util.show_document'
-  -- so that LSP entries are added to the tag stack
-  if not isURI and force_uri then
+  --Force LSP jumps using `vim.lsp.util.show_document` so that LSP entries are
+  --added to the tag stack (see `:help gettagstack`)
+  if not isURI and opts._uri then
     isURI = true
     stripped = "file://" .. stripped
   end
-  -- entries from 'buffers' contain '[<bufnr>]'
-  -- buffer placeholder always comes before the nbsp
-  local bufnr = idx > 1 and entry:sub(1, idx):match("%[(%d+)") or nil
+  -- Entry metadata (before `utils.nbsp`) can contain `[bufnr]` which should
+  -- be used instead of the file path, used in buffers, tabs, lines|blines
+  local bufnr = idx > 1 and entry:sub(1, idx):match("%[(%d+)%]") or nil
   if isURI and not bufnr then
-    -- Issue #195, when using nvim-jdtls
-    -- https://github.com/mfussenegger/nvim-jdtls
-    -- LSP entries inside .jar files appear as URIs
-    -- 'jdt://' which can then be opened with
-    -- 'vim.lsp.util.show_document' or
-    -- 'lua require('jdtls').open_jdt_link(vim.fn.expand('jdt://...'))'
-    -- Convert to location item so we can use 'jump_to_location'
-    -- This can also work with any 'file://' prefixes
+    -- LSP entries can appear as URIs, for example when using nvim-jdtls
+    -- references inside ".jar" files will have a prefix of "jdt://..."
+    -- we also "hack" our LSP entries to appear as URIs by prefixing the
+    -- entry with "file:// and then converting the entry to a URI that can
+    -- be used with `vim.lsp.utils.show_document` which adds the jump to
+    -- neovim's tagstack (see `:help gettagstack`)
     return M.entry_to_location(stripped, opts)
   end
   local s = utils.strsplit(stripped, ":")
@@ -568,9 +566,13 @@ function M.keymap_to_entry(str, opts)
   if not mode or not keymap then return {} end
   mode, keymap = vim.trim(mode), vim.trim(keymap)
   mode = valid_modes[mode] and mode or "" -- only valid modes
-  local vmap = utils.strsplit(
-    vim.fn.execute(string.format("verbose %smap %s", mode, keymap)), "\n")[1]
-  local out = utils.strsplit(vmap, "\n")
+  local out, vmap, cmd = nil, nil, string.format("verbose %smap %s", mode, keymap)
+  -- Run in the context of the originating buffer or keympas might return
+  -- "No mapping found"
+  pcall(vim.api.nvim_buf_call, opts.__CTX.bufnr, function()
+    out = utils.strsplit(vim.fn.execute(cmd), "\n")
+    _, vmap = next(vim.tbl_map(function(x) return #x > 0 and x or nil end, out))
+  end)
   local entry
   for i = #out, 1, -1 do
     if out[i]:match(utils.lua_regex_escape(keymap)) then

lua/fzf-lua/config.lua
@@ -793,7 +793,7 @@ function M.normalize_opts(opts, globals, __resume_key)
 end
 
 M.bytecode = function(s, datatype)
-  local keys = utils.strsplit(s, ".")
+  local keys = utils.strsplit(s, "%.")
   local iter = M
   for i = 1, #keys do
     iter = iter[keys[i]]

lua/fzf-lua/make_entry.lua
@@ -119,7 +119,7 @@ M.glob_parse = function(query, opts)
   end
   local glob_args = ""
   local search_query, glob_str = query:match("(.*)" .. opts.glob_separator .. "(.*)")
-  for _, s in ipairs(utils.strsplit(glob_str, "%s")) do
+  for _, s in ipairs(utils.strsplit(glob_str, "%s+")) do
     glob_args = glob_args .. ("%s %s "):format(opts.glob_flag, libuv.shellescape(s))
   end
   return search_query, glob_args

lua/fzf-lua/utils.lua
@@ -119,19 +119,12 @@ end
 ---@return string[]
 M.strsplit = function(inputstr, sep)
   local t = {}
-  -- Also match any single character class (e.g. %s, %a, etc)
-  if #sep == 1 or sep:match("^%%.$") then
-    for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
-      table.insert(t, str)
-    end
-  else
-    local s, m, r = inputstr, nil, nil
-    repeat
-      m, r = s:match("^(.-)" .. sep .. "(.*)$")
-      s = r and r or s
-      table.insert(t, m or s)
-    until not m
-  end
+  local s, m, r = inputstr, nil, nil
+  repeat
+    m, r = s:match("^(.-)" .. sep .. "(.*)$")
+    s = r and r or s
+    table.insert(t, m or s)
+  until not m
   return t
 end
 
@@ -438,7 +431,7 @@ end
 function M.map_get(m, k)
   if not m then return end
   if not k then return m end
-  local keys = type(k) == "table" and k or M.strsplit(k, ".")
+  local keys = type(k) == "table" and k or M.strsplit(k, "%.")
   local iter = m
   for i = 1, #keys do
     iter = iter[keys[i]]
@@ -460,7 +453,7 @@ end
 ---@return table<string, unknown>
 function M.map_set(m, k, v)
   m = m or {}
-  local keys = type(k) == "table" and k or M.strsplit(k, ".")
+  local keys = type(k) == "table" and k or M.strsplit(k, "%.")
   local map = m
   for i = 1, #keys do
     local key = keys[i]

commit b4153da4825f86c7dbc540c5fc238ccc8c24d75a

fix(bat): regression from d99996b (closes #1651)

lua/fzf-lua/actions.lua
@@ -135,7 +135,7 @@ M.vimcmd_entry = function(_vimcmd, selected, opts, pcall_vimcmd)
   for i, sel in ipairs(selected) do
     (function()
       -- Lua 5.1 goto compatiblity hack (function wrap)
-      local entry = path.entry_to_file(sel, opts)
+      local entry = path.entry_to_file(sel, opts, opts._uri)
       -- "<none>" could be set by `autocmds`
       if entry.path == "<none>" then return end
       local fullpath = entry.bufname or entry.uri and entry.uri:match("^%a+://(.*)") or entry.path

lua/fzf-lua/path.lua
@@ -408,7 +408,7 @@ function M.entry_to_location(entry, opts)
   }
 end
 
-function M.entry_to_file(entry, opts)
+function M.entry_to_file(entry, opts, force_uri)
   opts = opts or {}
   if opts._fmt then
     if type(opts._fmt._from) == "function" then
@@ -430,7 +430,7 @@ function M.entry_to_file(entry, opts)
   end
   --Force LSP jumps using `vim.lsp.util.show_document` so that LSP entries are
   --added to the tag stack (see `:help gettagstack`)
-  if not isURI and opts._uri then
+  if not isURI and force_uri then
     isURI = true
     stripped = "file://" .. stripped
   end