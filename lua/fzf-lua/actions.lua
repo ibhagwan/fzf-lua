7e63bc8a61ef9f38b513a19718308e57f4729d14
Ken Maeshima <81536479+mononoken@users.noreply.github.com>
2025-01-09 16:56:32 -0800

    feat(buffers): close dirty buffer dialog (#1697)

--- lua/fzf-lua/actions.lua
+++ lua/fzf-lua/actions.lua
@@ -405,8 +405,15 @@ M.buf_switch_or_edit = M.file_switch_or_edit
 M.buf_del = function(selected, opts)
   for _, sel in ipairs(selected) do
     local entry = path.entry_to_file(sel, opts)
-    if entry.bufnr and not utils.buffer_is_dirty(entry.bufnr, true, false) then
-      vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+    if entry.bufnr then
+      if not utils.buffer_is_dirty(entry.bufnr, true, false) then
+        vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+      elseif vim.api.nvim_buf_call(entry.bufnr, function()
+            return utils.save_dialog(entry.bufnr)
+          end)
+      then
+        vim.api.nvim_buf_delete(entry.bufnr, { force = true })
+      end
     end
   end
 end
