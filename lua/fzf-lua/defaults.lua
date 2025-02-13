3c5dca3c2bba49b6f7e7be62525a2002e83a2943
Simon Ho <simonho.ubc@gmail.com>
2025-02-13 01:43:18 -0800

    feat(options): keymap for setting local and global scoped options

--- lua/fzf-lua/defaults.lua
+++ lua/fzf-lua/defaults.lua
@@ -1062,8 +1062,10 @@ M.defaults.options              = {
   previewer     = { _ctor = previewers.builtin.options },
   separator     = "│",
   color_values  = true,
-  nvim_set_opts = { scope = "local" },
-  actions       = { ["enter"] = { fn = actions.option_edit, reload = true } },
+  actions       = {
+    ["enter"] = { fn = function(selected, opts) actions.option_edit(selected, opts, "local") end, reload = true },
+    ["alt-enter"] = { fn = function(selected, opts) actions.option_edit(selected, opts, "global") end, reload = true },
+  },
   fzf_opts      = {
     ["--nth"] = 1,
     ["--delimiter"] = "[│]",
