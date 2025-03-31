96d0aeb27eb5a723c7d39e1f813e8e8146a2248a
Zwlin <zwlin98@outlook.com>
2025-03-31 17:22:24 +0800

    feat(buffers|blines): support search specified range of current buffer

--- lua/fzf-lua/providers/buffers.lua
+++ lua/fzf-lua/providers/buffers.lua
@@ -312,10 +312,15 @@ M.buffer_lines = function(opts)
           return bname, bicon and bicon .. utils.nbsp or nil
         end)()
 
-        local offset, lines = 0, #data
-        if opts.current_buffer_only and opts.start == "cursor" then
-          -- start display from current line and wrap from bottom (#822)
-          offset = core.CTX().cursor[1] - 1
+        local offset, start_line, end_line, lines = 0, 1, #data, #data
+        if opts.current_buffer_only then
+          start_line = opts.start_line or 1
+          end_line = opts.end_line or end_line
+          lines = end_line - start_line + 1
+          if opts.start == "cursor" then
+            -- start display from current line and wrap from bottom (#822)
+            offset = core.CTX().cursor[1] - start_line
+          end
         end
 
         for i = 1, lines do
@@ -323,6 +328,7 @@ M.buffer_lines = function(opts)
           if lnum > lines then
             lnum = lnum % lines
           end
+          lnum = lnum + start_line - 1
 
           -- NOTE: Space after `lnum` is U+00A0 (decimal: 160)
           add_entry(string.format("[%s]\t%s\t%s%s\t%s \t%s",
