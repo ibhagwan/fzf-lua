ibhagwan/fzf-lua

commit 23aaad0339471d408cfe6875ad16747b07ce5be3

fix(keymap): lowercase keymap override (closes #2145)

lua/fzf-lua/config.lua
@@ -79,17 +79,21 @@ M.globals = setmetatable({}, {
       -- bind tables are logical exception, do not merge with defaults unless `[1] == true`
       -- normalize all binds as lowercase to prevent duplicate keys (#654)
       local ret = {}
+      -- exclude case-sensitive alt-binds from being lowercased
+      local exclude_case_sensitive_alt = "^alt%-%a$"
       for _, k in ipairs(keys) do
-        ret[k] = setup_value and type(setup_value[k]) == "table"
-            and vim.tbl_deep_extend("keep", utils.tbl_deep_clone(setup_value[k]),
-              setup_value[k][1] == true and fzflua_default[k] or {})
-            or utils.tbl_deep_clone(fzflua_default[k])
-        if ret[k] then
+        local inherit_defaults
+        if setup_value and type(setup_value[k]) == "table" and setup_value[k][1] == true then
           -- Remove the [1] indicating inheritance from defaults and
-          -- exclude case-sensitive alt-binds from being lowercased
-          ret[k][1] = nil
-          ret[k] = utils.map_tolower(ret[k], "^alt%-%a$")
+          setup_value[k][1] = nil
+          inherit_defaults = true
         end
+        ret[k] = setup_value and type(setup_value[k]) == "table"
+            and vim.tbl_deep_extend("keep",
+              utils.map_tolower(utils.tbl_deep_clone(setup_value[k]), exclude_case_sensitive_alt),
+              inherit_defaults and
+              utils.map_tolower(fzflua_default[k], exclude_case_sensitive_alt) or {})
+            or utils.map_tolower(utils.tbl_deep_clone(fzflua_default[k]), exclude_case_sensitive_alt)
       end
       return ret
     end

commit bc64c22cb0b0bd2c18e5e541eb845acfc170d37a

fix: regression from 23aaad0

lua/fzf-lua/config.lua
@@ -82,18 +82,16 @@ M.globals = setmetatable({}, {
       -- exclude case-sensitive alt-binds from being lowercased
       local exclude_case_sensitive_alt = "^alt%-%a$"
       for _, k in ipairs(keys) do
-        local inherit_defaults
-        if setup_value and type(setup_value[k]) == "table" and setup_value[k][1] == true then
-          -- Remove the [1] indicating inheritance from defaults and
-          setup_value[k][1] = nil
-          inherit_defaults = true
-        end
         ret[k] = setup_value and type(setup_value[k]) == "table"
             and vim.tbl_deep_extend("keep",
               utils.map_tolower(utils.tbl_deep_clone(setup_value[k]), exclude_case_sensitive_alt),
-              inherit_defaults and
+              setup_value[k][1] == true and
               utils.map_tolower(fzflua_default[k], exclude_case_sensitive_alt) or {})
             or utils.map_tolower(utils.tbl_deep_clone(fzflua_default[k]), exclude_case_sensitive_alt)
+        if ret[k] and ret[k][1] == true then
+          -- Remove the [1] indicating inheritance from defaults and
+          ret[k][1] = nil
+        end
       end
       return ret
     end

lua/fzf-lua/utils.lua
@@ -492,6 +492,9 @@ function M.map_tolower(m, exclude_patterns)
   local ret = {}
   for k, v in pairs(m) do
     local lower_k = (function()
+      if type(k) ~= "string" then
+        return k
+      end
       for _, p in ipairs(exclude_patterns) do
         if k:match(p) then return k end
       end