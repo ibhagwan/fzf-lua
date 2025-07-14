4b26f624e0a0f80acef52ba45bbefc60cbe1102b
bhagwan <bhagwan@disroot.org>
2025-07-13 19:35:24 -0700

    ci: added testing for `fzf_live` API

--- tests/api_spec.lua
+++ tests/api_spec.lua
@@ -12,8 +12,7 @@ T["api"] = new_set()
 T["api"]["fzf_exec"] = new_set()
 
 T["api"]["fzf_exec"]["table"] = function()
-  helpers.FzfLua.fzf_exec(child,
-    [==[(function()
+  helpers.FzfLua.fzf_exec(child, [==[(function()
       local contents = {}
       for i = 1, 100 do
         for j, s in ipairs({ "foo", "bar", "baz" }) do
@@ -44,9 +43,82 @@ T["api"]["fzf_exec"]["function"] = new_set({ parametrize = { { "sync" }, { "asyn
             coroutine.yield()
           end
           fzf_cb(nil)
-        end))
-        ]==],
+        end))]==],
         { __expect_lines = true, __postprocess_wait = true })
+    end
+  end,
+})
+
+T["api"]["fzf_live"] = new_set()
+
+T["api"]["fzf_live"]["table"] = function()
+  helpers.FzfLua.fzf_live(child, [==[function(args)
+      local q = args[1]
+      if not tonumber(q) then
+        return { "Invalid number: " .. tostring(q) }
+      end
+      local lines = {}
+      for i = 1, tonumber(q) do
+        for j, s in ipairs({ "foo", "bar", "baz" }) do
+          table.insert(lines, tostring((i - 1) * 3 + j) .. ": " .. s)
+        end
+      end
+      return lines
+    end]==],
+    {
+      __expect_lines = true,
+      __postprocess_wait = true,
+      query = 100,
+    })
+end
+
+T["api"]["fzf_live"]["function"] = new_set({ parametrize = { { "sync" }, { "async" } } }, {
+  function(type)
+    if type == "sync" then
+      helpers.FzfLua.fzf_live(child, [==[function(args)
+          local q = args[1]
+          return function(fzf_cb)
+            if not tonumber(q) then
+              fzf_cb("Invalid number: " .. tostring(q))
+            else
+              for i = 1, tonumber(q) do
+                for j, s in ipairs({ "foo", "bar", "baz" }) do
+                  fzf_cb(tostring((i - 1) * 3 + j) .. ": " .. s)
+                end
+              end
+            end
+            fzf_cb(nil)
+          end
+        end]==],
+        {
+          __expect_lines = true,
+          __postprocess_wait = true,
+          query = 100,
+        })
+    else
+      helpers.FzfLua.fzf_live(child, [==[function(args)
+          local q = args[1]
+          return coroutine.wrap(function(fzf_cb)
+            local co = coroutine.running()
+            if not tonumber(q) then
+              fzf_cb("Invalid number: " .. tostring(q), function() coroutine.resume(co) end)
+              coroutine.yield()
+            else
+              for i = 1, tonumber(q) do
+                for j, s in ipairs({ "foo", "bar", "baz" }) do
+                  fzf_cb(tostring((i - 1) * 3 + j) .. ": " .. s, function() coroutine.resume(co) end)
+                  coroutine.yield()
+                end
+              end
+            end
+            fzf_cb(nil)
+          end)
+        end]==],
+        {
+          __expect_lines = true,
+          __postprocess_wait = true,
+          query = 100,
+        })
     end
   end,
 })
