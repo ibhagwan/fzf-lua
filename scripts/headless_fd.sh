53cf12a867fb23578544dfd68571e62dc9ed3ccf
bhagwan <bhagwan@disroot.org>
2025-07-20 10:39:46 -0700

    ci: headless script tests

--- scripts/headless_fd.sh
+++ scripts/headless_fd.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BASEDIR=$(cd "$(dirname "$0")" ; pwd -P)
+GETOPT=getopt
 
 usage() {
     echo "Usage: $0 [options]"
@@ -12,26 +13,31 @@ usage() {
     echo "Display Options"
     echo "-c, --cwd             Working Directory"
     echo "-x, --cmd             Executed Command (default: fd --color=never)"
-    echo "-g, --git-icons       Git icons [0|1|false|true] (default:false)"
-    echo "-f, --file-icons      File icons [0|1|false|true] (default:true)"
-    echo "--color               Color icons [0|1|false|true] (default:true)"
+    echo "-g, --git-icons       Git icons [false|true] (default:false)"
+    echo "-f, --file-icons      File icons [false|true] (default:true)"
+    echo "--color               Color icons [false|true] (default:true)"
 }
 
 # saner programming env: these switches turn some bugs into errors
 set -o noclobber -o nounset
 
+# MacOs: `brew install gnu-getopt && brew link --force gnu-getopt`
+if [ -x /usr/local/bin/getopt ]; then
+    GETOPT=/usr/local/bin/getopt
+fi
+
 # -allow a command to fail with !’s side effect on errexit
 # -use return value from ${PIPESTATUS[0]}, because ! hosed $?
-getopt --test > /dev/null 
+$GETOPT --test > /dev/null 
 if [ $? -ne 4 ]; then
-    echo '`getopt --test` failed in this environment.'
+    echo "`$GETOPT --test` failed in this environment."
     exit 1
 fi
 
 OPTIONS=hd:c:x:f:g:
 LONGOPTS=help,debug:,cwd:,file-icons:,git-icons:,color:,cmd:
 
-PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+PARSED=$($GETOPT --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [ $? -ne 0 ]; then
     # e.g. return value is 1
     #  then getopt has complained about wrong arguments to stdout
@@ -55,17 +61,7 @@ while true; do
             exit 0
             ;;
         -d|--debug)
-            case $2 in
-                "2"|"v"|"verbose")
-                    debug="v"
-                    ;;
-                "1"|"true")
-                    debug="true"
-                    ;;
-                *)
-                    debug="false"
-                    ;;
-            esac
+            debug="$2"
             shift 2
             ;;
         -c|--cwd)
@@ -78,35 +74,21 @@ while true; do
             ;;
         -f|--file-icons)
             case $2 in
-                "0"|"false")
-                    file_icons="false"
+                "true"|"srv"|"mini"|"devicons")
+                    file_icons="$2"
                     ;;
                 *)
-                    file_icons="true"
+                    file_icons="nil"
                     ;;
             esac
             shift 2
             ;;
         -g|--git-icons)
-            case $2 in
-                "0"|"false")
-                    git_icons="false"
-                    ;;
-                *)
-                    git_icons="true"
-                    ;;
-            esac
+            git_icons="$2"
             shift 2
             ;;
         --color)
-            case $2 in
-                "0"|"false")
-                    color_icons="false"
-                    ;;
-                *)
-                    color_icons="true"
-                    ;;
-            esac
+            color_icons="$2"
             shift 2
             ;;
         --)
@@ -129,18 +111,38 @@ if [ $# -gt 0 ]; then
     exit 4
 fi
 
-VIMRUNTIME=/usr/share/nvim/runtime \
-/usr/bin/nvim -u NONE -l ${BASEDIR}/../lua/fzf-lua/spawn.lua "return
+ICONS_PATH=nil
+ICONS_SETUP=nil
+
+if [ $file_icons != "nil" ]; then
+    if [ $file_icons != "mini" ]; then
+        if [ -d "${BASEDIR}/../deps/nvim-web-devicons" ]; then
+            ICONS_PATH="${BASEDIR}/../deps/nvim-web-devicons"
+        else
+            ICONS_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/lazy/nvim-web-devicons"
+        fi
+        ICONS_SETUP="${BASEDIR}/devicons.lua"
+    else
+        if [ -d "${BASEDIR}/../deps/mini.nvim" ]; then
+            ICONS_PATH="${BASEDIR}/../deps/mini.nvim"
+        else
+            ICONS_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/lazy/mini.nvim"
+        fi
+    fi
+fi
+
+# VIMRUNTIME=/usr/share/nvim/runtime \
+nvim -u NONE -l ${BASEDIR}/../lua/fzf-lua/spawn.lua "return
   -- opts
   {
     g = {
-      --_fzf_lua_server = [[/run/user/1000/fzf-lua.1710687343.12851.1]],
-      _devicons_path = [[${XDG_DATA_HOME:-$HOME/.local/share}/nvim/lazy/nvim-web-devicons]],
-      _devicons_setup = [[${XDG_CONFIG_HOME:-$HOME/.config}/nvim/lua/plugins/devicons/setup.lua]],
+      _fzf_lua_server = #[[${FZF_LUA_SERVER:-}]] > 0 and [[${FZF_LUA_SERVER:-}]] or nil,
+      _devicons_path = [[${ICONS_PATH}]],
+      _devicons_setup = [[${ICONS_SETUP}]],
     },
     _base64 = false,
     debug = [[$debug]] == [[v]] and [[v]] or $debug,
-    file_icons = ${file_icons},
+    file_icons = [[${file_icons}]] == true or [[${file_icons}]],
     git_icons = ${git_icons},
     color_icons = ${color_icons},
     cmd = [[${cmd:-fd --color=never}]],
---
+++ tests/headless_spec.lua
@@ -0,0 +1,90 @@
+---@diagnostic disable: unused-local, unused-function
+local MiniTest = require("mini.test")
+local helpers = require("fzf-lua.test.helpers")
+local child = helpers.new_child_neovim()
+local expect, eq = helpers.expect, helpers.expect.equality
+local new_set = MiniTest.new_set
+
+local _mini_path = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "mini.nvim")
+if not vim.uv.fs_stat(_mini_path) then
+  _mini_path = vim.fs.joinpath("deps", "mini.nvim")
+end
+
+local _devicons_path = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "nvim-web-devicons")
+if not vim.uv.fs_stat(_devicons_path) then
+  _devicons_path = vim.fs.joinpath("deps", "nvim-web-devicons")
+end
+
+local T = helpers.new_set_with_child(child, {
+  hooks = {
+    pre_case = function()
+      child.o.shell = "/bin/sh"
+      child.o.termguicolors = true
+      child.o.background = "dark"
+      child.lua([[M = { devicons = require("fzf-lua.devicons") }]])
+    end,
+  },
+})
+
+T["headless"] = new_set({})
+
+T["headless"]["file_icons"] = new_set()
+
+local exec_term = function(c, cmd, args)
+  cmd = cmd or {}
+  args = args or {}
+  args.term = true
+  local serpent = require "fzf-lua.lib.serpent"
+  cmd = serpent.block(cmd, { comment = false, sortkeys = false })
+  args = serpent.block(args, { comment = false, sortkeys = false })
+  local id = c.lua_get(string.format("vim.fn.jobstart(%s, %s)", cmd, args))
+  eq(tonumber(id) > 0, true)
+  c.lua(string.format("vim.fn.jobwait({%s})", tostring(id)))
+  c.wait_until(function()
+    local lines = c.get_lines()
+    for i = #lines, 1, -1 do
+      if lines[i] == "" then
+        table.remove(lines, i)
+      end
+    end
+    return lines[#lines] == "[Process exited 0]"
+  end)
+end
+
+T["headless"]["file_icons"]["devicons - manual"] = new_set({ parametrize = { { false }, { true } } },
+  {
+    function(icons)
+      helpers.SKIP_IF_WIN()
+      -- helpers.SKIP_IF_NOT_LINUX()
+      exec_term(child,
+        { "./scripts/headless_fd.sh", "-x", "rg --files --sort=path", "-f", tostring(icons) })
+      child.expect_screenshot({ ignore_text = { 24 }, ignore_attr = { 25 } })
+    end,
+  })
+
+T["headless"]["file_icons"]["server"] = new_set({ parametrize = { { "devicons" }, { "mini" } } }, {
+  function(icons)
+    helpers.SKIP_IF_WIN()
+    -- helpers.SKIP_IF_NOT_LINUX()
+    if icons == "mini" then
+      child.lua("vim.opt.runtimepath:append(...)", { _mini_path })
+      child.lua([[require("mini.icons").setup({})]])
+    else
+      child.lua("vim.opt.runtimepath:append(...)", { _devicons_path })
+      child.lua([[ require("nvim-web-devicons").setup({})]])
+    end
+    child.lua([[M.devicons.load()]])
+    eq(child.lua_get([[M.devicons.plugin_name()]]), icons)
+    local fzf_lua_server = child.lua_get("vim.g.fzf_lua_server")
+    eq(#fzf_lua_server > 0, true)
+    local new_child = helpers.new_child_neovim()
+    new_child.start()
+    exec_term(new_child,
+      { "./scripts/headless_fd.sh", "-x", "rg --files --sort=path", "-f", icons },
+      { env = { ["FZF_LUA_SERVER"] = fzf_lua_server } })
+    -- Ignore script path and attr the next line
+    new_child.expect_screenshot({ ignore_text = { 23 }, ignore_attr = { 24 } })
+  end,
+})
+
+return T
