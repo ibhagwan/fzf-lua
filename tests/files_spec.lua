ibhagwan/fzf-lua

commit f0e830e278ad954a4e545928f8bfe08725a71173

refactor: patch_shell_action -> shell.stringify_data2

lua/fzf-lua/core.lua
@@ -957,45 +957,6 @@ M.set_header = function(opts, hdr_tbl)
   return opts
 end
 
-
--- Use both {q} and {+} as field indexes so we can update last query when
--- executing the action, without this we lose the last query on "hide" as
--- the process never terminates and `--print-query` isn't being printed
--- When no entry selected (with {q} {+}), {+} will be forced expand to ''
--- Use {n} to know if we really select an empty string, or there's just no selected
-local patch_shell_action = function(v, opts)
-  local field_index = v.field_index == false and "" or v.field_index or "{+}"
-  local overide_f_idx
-  if not field_index:match("^{q} {n}") then
-    field_index = "{q} {n} " .. field_index
-    overide_f_idx = true
-  end
-  -- replace the action with shell cmd proxy to the original action
-  return shell.stringify_data(function(items, _, _)
-    assert(field_index:match("^{q} {n}"))
-    local query, idx = unpack(items, 1, 2)
-    config.resume_set("query", query, opts)
-    if overide_f_idx then
-      table.remove(items, 1)
-      table.remove(items, 1)
-    end
-    -- fix side effect of "{q} {+}": {+} is forced expanded to ""
-    -- only when: user didn't set v.field_index (otherwise it can be complex/unpredictable)
-    -- {n} used to determine if "zero-selected && zero-match", then patch: "" -> nil
-    if not v.field_index then
-      -- When no item is matching (empty list or non-matching query)
-      -- both {n} and {+} are expanded to "".
-      -- NOTE1: older versions of fzf don't expand {n} to "" (without match)
-      -- in such case the (empty) items table will be in `items[2]` (#1833)
-      -- NOTE2: on Windows, no match {n} is expanded to '' (#1836)
-      local zero_matched = not tonumber(idx)
-      local zero_selected = #items == 0 or (#items == 1 and #items[1] == 0)
-      items = (zero_matched and zero_selected) and {} or items
-    end
-    v.fn(items, opts)
-  end, opts, field_index)
-end
-
 -- converts actions defined with "reload=true" to use fzf's `reload` bind
 -- provides a better UI experience without a visible interface refresh
 ---@param reload_cmd string?
@@ -1057,18 +1018,18 @@ M.convert_reload_actions = function(reload_cmd, opts)
     if type(v) == "table" and v.reload then
       -- Modified actions should not be considered in `actions.expect`
       opts.actions[k]._ignore = true
-      local shell_action = patch_shell_action(v, opts)
       if type(v.prefix) == "string" and not v.prefix:match("%+$") then
         v.prefix = v.prefix .. "+"
       end
       if type(v.postfix) == "string" and not v.postfix:match("^%+") then
         v.postfix = "+" .. v.postfix
       end
+      local cmd = shell.stringify_data2(v.fn, opts, v.field_index or "{+}")
       opts.keymap.fzf[k] = {
         string.format("%s%sexecute-silent(%s)+reload(%s)%s",
           type(v.prefix) == "string" and v.prefix or "",
           unbind and (unbind .. "+") or "",
-          shell_action,
+          cmd,
           reload_cmd,
           type(v.postfix) == "string" and v.postfix or ""),
         desc = v.desc or config.get_action_helpstr(v.fn)
@@ -1097,7 +1058,6 @@ M.convert_exec_silent_actions = function(opts)
       assert(type(v.fn) == "function")
       -- Modified actions should not be considered in `actions.expect`
       opts.actions[k]._ignore = true
-      local shell_action = patch_shell_action(v, opts)
       if type(v.prefix) == "string" and not v.prefix:match("%+$") then
         v.prefix = v.prefix .. "+"
       end
@@ -1117,13 +1077,14 @@ M.convert_exec_silent_actions = function(opts)
       --      key-action pairs.
       --
       local has_fzf036 = utils.has(opts, "fzf", { 0, 36 })
+      local cmd = shell.stringify_data2(v.fn, opts, v.field_index or "{+}")
       opts.keymap.fzf[k] = {
         string.format("%sexecute-silent%s%s",
           type(v.prefix) == "string" and v.prefix or "",
           -- prefer "execute-silent:..." unless we have postfix
           has_fzf036 and type(v.postfix) == "string"
-          and string.format("(%s)", shell_action)
-          or string.format(":%s", shell_action),
+          and string.format("(%s)", cmd)
+          or string.format(":%s", cmd),
           -- can't use postfix since we use "execute-silent:..."
           has_fzf036 and type(v.postfix) == "string" and v.postfix or ""),
         desc = v.desc or config.get_action_helpstr(v.fn)

lua/fzf-lua/shell.lua
@@ -306,9 +306,9 @@ M.stringify_mt = function(cmd, opts)
   end
 end
 
+---Fzf field index expression, e.g. "{+}" (selected), "{q}" (query)
 ---@param contents table|function|string
 ---@param opts {}
----Fzf field index expression, e.g. "{+}" (selected), "{q}" (query)
 ---@param fzf_field_index string?
 ---@return string?, integer?
 M.stringify = function(contents, opts, fzf_field_index)
@@ -498,7 +498,7 @@ M.stringify = function(contents, opts, fzf_field_index)
   return cmd, id
 end
 
----@param fn fun(item: string[], fzf_lines: integer, fzf_columns, integer): string|string[]?
+---@param fn fun(items: string[], fzf_lines: integer, fzf_columns: integer): string|{ cmd: string|string[], env: table? }?
 ---@param opts table
 ---@param fzf_field_index string?
 ---@return string, integer?
@@ -511,6 +511,10 @@ M.stringify_cmd = function(fn, opts, fzf_field_index)
   }, fzf_field_index)
 end
 
+---@param fn fun(items: string[], fzf_lines: integer, fzf_columns: integer): string|string[]?
+---@param opts table
+---@param fzf_field_index string?
+---@return string, integer?
 M.stringify_data = function(fn, opts, fzf_field_index)
   assert(type(fn) == "function", "fn must be of type function")
   return M.stringify(function(cb, _, ...)
@@ -524,6 +528,48 @@ M.stringify_data = function(fn, opts, fzf_field_index)
     end
     cb(nil)
   end, { debug = opts.debug }, fzf_field_index)
+end
+
+-- Patched version of stringify_data
+-- Use both {q} and {+} as field indexes so we can update last query when
+-- executing the action, without this we lose the last query on "hide" as
+-- the process never terminates and `--print-query` isn't being printed
+-- When no entry selected (with {q} {+}), {+} will be forced expand to ''
+-- Use {n} to know if we really select an empty string, or there's just no selected
+---@param fn fun(items: string[], opts: table): string|string[]?
+---@param opts table
+---@param field_index string
+---@return string
+M.stringify_data2 = function(fn, opts, field_index)
+  local field_index0 = field_index
+  local did_override = false
+  if not field_index:match("{q} {n}$") then
+    field_index = field_index .. " {q} {n}"
+    did_override = true
+  end
+  -- replace the action with shell cmd proxy to the original action
+  return M.stringify_data(function(items, _, _)
+    local query, idx = items[#items - 1], items[#items]
+    FzfLua.config.resume_set("query", query, opts)
+    if did_override then
+      table.remove(items)
+      table.remove(items)
+    end
+    -- fix side effect of "{q} {+}": {+} is forced expanded to ""
+    -- only when field_index is empty (otherwise it can be complex/unpredictable)
+    -- {n} used to determine if "zero-selected && zero-match", then patch: "" -> nil
+    if #field_index0 == 0 then
+      -- When no item is matching (empty list or non-matching query)
+      -- both {n} and {+} are expanded to "".
+      -- NOTE1: older versions of fzf don't expand {n} to "" (without match)
+      -- in such case the (empty) items table will be in `items[2]` (#1833)
+      -- NOTE2: on Windows, no match {n} is expanded to '' (#1836)
+      local zero_matched = not tonumber(idx)
+      local zero_selected = #items == 0 or (#items == 1 and #items[1] == 0)
+      items = (zero_matched and zero_selected) and {} or items
+    end
+    fn(items, opts)
+  end, opts, field_index)
 end
 
 return M

lua/fzf-lua/previewer/builtin.lua
@@ -443,6 +443,7 @@ end
 
 function Previewer.base:cmdline(_)
   local act = shell.stringify_data(function(items, _, _)
+    ---@type string?, string?, string?
     local entry, query, idx = unpack(items, 1, 3)
     -- NOTE: see comment regarding {n} in `core.convert_exec_silent_actions`
     -- convert empty string to nil

commit f7ab756cc6a948f419769d22f44c3e9ddfa2dec3

fix(hide): actions should be nop when nothing match
Regression from f0e830e278ad954a4e545928f8bfe08725a71173

e.g. `FzfLua files query=nothing` then `<c-t>` should not open new tab

fix: opts.__pid -> opts.PidObject

lua/fzf-lua/core.lua
@@ -392,8 +392,9 @@ M.fzf = function(contents, opts)
   -- NOTE: might be an overkill since we're using $FZF_DEFAULT_COMMAND
   -- to spawn the piped process and fzf is responsible for termination
   -- when the fzf process exists
-  if tonumber(opts.__pid) then
-    libuv.process_kill(opts.__pid)
+  if opts.PidObject then
+    libuv.process_kill(opts.PidObject:get())
+    opts.PidObject:set(nil)
   end
   -- If a hidden process was killed by [re-]starting a new picker do nothing
   if fzf_win:was_hidden() then return nil, nil end

lua/fzf-lua/shell.lua
@@ -489,7 +489,6 @@ end
 ---@param field_index string
 ---@return string
 M.stringify_data2 = function(fn, opts, field_index)
-  local field_index0 = field_index
   local did_override = false
   if not field_index:match("{q} {n}$") then
     field_index = field_index .. " {q} {n}"
@@ -506,7 +505,7 @@ M.stringify_data2 = function(fn, opts, field_index)
     -- fix side effect of "{q} {+}": {+} is forced expanded to ""
     -- only when field_index is empty (otherwise it can be complex/unpredictable)
     -- {n} used to determine if "zero-selected && zero-match", then patch: "" -> nil
-    if #field_index0 == 0 then
+    if field_index == "{+} {q} {n}" then
       -- When no item is matching (empty list or non-matching query)
       -- both {n} and {+} are expanded to "".
       -- NOTE1: older versions of fzf don't expand {n} to "" (without match)

tests/files_spec.lua
@@ -87,7 +87,7 @@ T["files"]["previewer"]["builtin"] = new_set({ parametrize = { { "ci" }, { "buil
         -- Verify previewer "last_entry" was set
         child.type_keys("<c-j>")
         child.wait_until(function()
-          return child.lua_get([[FzfLua.utils.fzf_winobj()._previewer.last_entry]]) ==
+          return exec_lua([[return FzfLua.utils.fzf_winobj()._previewer.last_entry]]) ==
               (icons and " LICENSE" or "LICENSE")
         end)
       end,
@@ -185,6 +185,18 @@ T["files"]["preview should work after chdir #1864"] = function()
       sleep(100)
     end
   })
+end
+
+T["files"]["nop on nothing match"] = function()
+  reload({ "hide" })
+  local ctx = exec_lua([[return FzfLua.utils.CTX()]])
+  for _, key in ipairs({ "<cr>", "<c-t>" }) do
+    exec_lua([[FzfLua.files { query = ("%s is nop on nothing match"):format(...) }]], { key })
+    child.wait_until(function() return exec_lua([[return _G._fzf_load_called]]) == true end)
+    child.type_keys(key)
+    child.wait_until(function() return exec_lua([[return _G._fzf_lua_on_create]]) == vim.NIL end)
+    eq(ctx, exec_lua([[return FzfLua.utils.CTX()]]))
+  end
 end
 
 return T