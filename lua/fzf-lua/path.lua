ibhagwan/fzf-lua

commit f82f986a7a7e4bc533c8a6a50bd5bb5d35ce4e76

fix: `Unable to stat file` after cwd change

lua/fzf-lua/path.lua
@@ -425,8 +425,9 @@ function M.entry_to_file(entry, opts, force_uri)
   stripped = M.tilde_to_HOME(stripped)
   -- Prepend cwd unless entry is already a URI (e.g. nvim-jdtls "jdt://...")
   local isURI = stripped:match("^%a+://")
-  if opts.cwd and #opts.cwd > 0 and not isURI and not M.is_absolute(stripped) then
-    stripped = M.join({ opts.cwd, stripped })
+  local cwd = opts.cwd or opts._cwd
+  if cwd and #cwd > 0 and not isURI and not M.is_absolute(stripped) then
+    stripped = M.join({ cwd, stripped })
   end
   --Force LSP jumps using `vim.lsp.util.show_document` so that LSP entries are
   --added to the tag stack (see `:help gettagstack`)

tests/file/ui_spec.lua
@@ -170,4 +170,20 @@ T["files()"]["executable"] = new_set({ parametrize = { { "fd" }, { "rg" }, { "fi
   end,
 })
 
+T["files()"]["preview should work after chdir #1864"] = function()
+  -- Ignore last "-- TERMINAL --" line and "[DEBUG]" line containing the cmd
+  local screen_opts = { ignore_lines = { 6, 28 }, normalize_paths = helpers.IS_WIN() }
+  eq(child.lua_get([[_G._fzf_lua_on_create]]), vim.NIL)
+  child.lua([[FzfLua.files {
+    cwd_prompt = false,
+    previewer = 'builtin',
+    winopts = { preview = { hidden = false  } }
+  }]])
+  child.wait_until(function() return child.lua_get([[_G._fzf_load_called]]) == true end)
+  child.lua([[vim.cmd.cd("./tests")]])
+  child.type_keys([[<c-n>]])
+  sleep(100)
+  child.expect_screen_lines(screen_opts)
+end
+
 return T

tests/screenshots/tests-file-ui_spec.lua---files()---executable---1-+-args-{-'fd'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭─────────────────── Files  h ────────────────────╮      
-04|~      │>                                    110/110 (0) │      
+04|~      │>                                    111/111 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌ [DEBUG] fd --hidden --color=never --type f -··││      
 07|~      │  .editorconfig                                 ││      

tests/screenshots/tests-file-ui_spec.lua---files()---executable---1-+-args-{-'find-dir'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭───────────────────── Files ─────────────────────╮      
-04|~      │>                                    110/110 (0) │      
+04|~      │>                                    111/111 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌ [DEBUG] find . -type f -not -path '*/\.git/*··││      
 07|~      │  .editorconfig                                 ││      

tests/screenshots/tests-file-ui_spec.lua---files()---executable---1-+-args-{-'rg'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭─────────────────── Files  h ────────────────────╮      
-04|~      │>                                    110/110 (0) │      
+04|~      │>                                    111/111 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌ [DEBUG] rg --hidden --files -g "!.git" --sor··││      
 07|~      │  .editorconfig                                 ││      

tests/screenshots/tests-file-ui_spec.lua---files()---icons---defaults---1-+-args-{-'devicons',-'+attrs'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭───────────────────── Files ─────────────────────╮      
-04|~      │>                                      98/98 (0) │      
+04|~      │>                                      99/99 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌  LICENSE                                     ││      
 07|~      │   Makefile                                    ││      

tests/screenshots/tests-file-ui_spec.lua---files()---icons---defaults---1-+-args-{-'devicons',-'-attrs'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭───────────────────── Files ─────────────────────╮      
-04|~      │>                                      98/98 (0) │      
+04|~      │>                                      99/99 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌  LICENSE                                     ││      
 07|~      │   Makefile                                    ││      

tests/screenshots/tests-file-ui_spec.lua---files()---icons---defaults---1-+-args-{-'mini',-'+attrs'-}
@@ -2,7 +2,7 @@
 01|                                                                
 02|~                                                               
 03|~      ╭───────────────────── Files ─────────────────────╮      
-04|~      │>                                      98/98 (0) │      
+04|~      │>                                      99/99 (0) │      
 05|~      │──────────────────────────────────────────────── │      
 06|~      │▌  LICENSE                                     ││      
 07|~      │  󱁤 Makefile                                    ││      

commit 263829d1c679972efe3c16d96adfe1a2b32b455d

fix: show debug information
regression from f82f986a

lua/fzf-lua/path.lua
@@ -524,6 +524,7 @@ function M.entry_to_file(entry, opts, force_uri)
       (opts.line_query(opts._last_query or opts.last_query)) or line) or 0,
     col      = tonumber(col) or 0,
     ctag     = opts._ctag and M.entry_to_ctag(stripped) or nil,
+    debug    = opts.debug and entry:match("^%[DEBUG]") and entry or nil,
   }
 end
 

lua/fzf-lua/previewer/builtin.lua
@@ -917,8 +917,8 @@ function Previewer.buffer_or_file:populate_preview_buf(entry_str)
     end
     do
       local lines = nil
-      if entry.path:match("^%[DEBUG]") then
-        lines = { tostring(entry.path:gsub("^%[DEBUG]", "")) }
+      if entry.debug then
+        lines = { entry.debug }
       elseif entry.content then
         lines = entry.content
       else
@@ -1285,16 +1285,12 @@ function Previewer.buffer_or_file:update_title(entry)
   if not self.title then return end
   local filepath = entry.path
   if filepath then
-    if filepath:match("^%[DEBUG]") then
-      filepath = "[DEBUG]"
-    else
-      if self.opts.cwd then
-        filepath = path.relative_to(entry.path, self.opts.cwd)
-      end
-      filepath = path.HOME_to_tilde(filepath)
+    if self.opts.cwd then
+      filepath = path.relative_to(entry.path, self.opts.cwd)
     end
+    filepath = path.HOME_to_tilde(filepath)
   end
-  local title = filepath or entry.uri or entry.bufname
+  local title = entry.debug and "[DEBUG]" or filepath or entry.uri or entry.bufname
   -- was transform function defined?
   if self.title_fnamemodify then
     local wincfg = vim.api.nvim_win_get_config(self.win.preview_winid)

lua/fzf-lua/previewer/fzf.lua
@@ -212,8 +212,8 @@ function Previewer.cmd_async:parse_entry_and_verify(entrystr)
     end
   end
   local errcmd = nil
-  if filepath:match("^%[DEBUG]") then
-    errcmd = "echo " .. libuv.shellescape(tostring(filepath:gsub("^%[DEBUG]", "")))
+  if entry.debug then
+    errcmd = "echo " .. libuv.shellescape(entry.debug)
   else
     -- verify the file exists on disk and is accessible
     if #filepath == 0 or not uv.fs_stat(filepath) then

lua/fzf-lua/types.lua
@@ -68,6 +68,7 @@ _G.FzfLua = require("fzf-lua")
 ---@field ctag string?
 ---@field uri string?
 ---@field range { start: { line: integer, col: integer } }?
+---@field debug string? debug information
 
 ---@class fzf-lua.buffer_or_file.Entry : fzf-lua.path.Entry, {}
 ---@field do_not_cache boolean?