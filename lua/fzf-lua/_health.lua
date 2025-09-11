ibhagwan/fzf-lua

commit 911d8d43081e91e41dba2f3dabf8d843eafba84f

fix(health): neovim without LuaJIT (#2324)

lua/fzf-lua/_health.lua
@@ -8,8 +8,8 @@ local error = vim.health.error or vim.health.report_error
 local uv = vim.uv or vim.loop
 
 function M.check()
-  local is_win = jit.os:find("Windows")
   local utils = require("fzf-lua.utils")
+  local is_win = M.__IS_WINDOWS
 
   local function have(tool, nowarn)
     if vim.fn.executable(tool) == 0 then

commit cb6ef028cacc1d855d152917993fefd12300f24e

fix(health): windows regression from 911d8d4

lua/fzf-lua/_health.lua
@@ -9,7 +9,7 @@ local uv = vim.uv or vim.loop
 
 function M.check()
   local utils = require("fzf-lua.utils")
-  local is_win = M.__IS_WINDOWS
+  local is_win = utils.__IS_WINDOWS
 
   local function have(tool, nowarn)
     if vim.fn.executable(tool) == 0 then