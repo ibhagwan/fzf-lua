ibhagwan/fzf-lua

commit a95354854d97af3f5bd2443ea28c26c97fe20312

fix: memoize has_ts_parser call
fix: duplicated entry

lua/fzf-lua/previewer/builtin.lua
@@ -966,7 +966,7 @@ local ts_attach = function(bufnr, ft)
   -- ts is already attach, see $VIMRUNTIME/lua/vim/treesitter/highlighter.lua
   if vim.b[bufnr].ts_highlight then return true end
   local lang = vim.treesitter.language.get_lang(ft)
-  local loaded = lang and utils.has_ts_parser(lang)
+  local loaded = lang and utils.has_ts_parser(lang, "highlights")
   if lang and loaded then
     local ok, err = pcall(vim.treesitter.start, bufnr, lang)
     if not ok then
@@ -997,7 +997,7 @@ function Previewer.base:update_ts_context()
   -- https://github.com/neovim/neovim/commit/45e606b1fddbfeee8fe28385b5371ca6f2fba71b
   -- For more info see #1922
   local lang = vim.treesitter.language.get_lang(ft)
-  if not utils.has_ts_parser(lang) then
+  if not utils.has_ts_parser(lang, "context") then
     TSContext.close(self.win.preview_winid)
     return
   end

lua/fzf-lua/providers/buffers.lua
@@ -466,8 +466,8 @@ M.treesitter = function(opts)
   local ts = vim.treesitter
   local ft = vim.bo[bufnr0].ft
   local lang = ts.language.get_lang(ft) or ft
-  if not utils.has_ts_parser(lang) then
-    utils.info("No treesitter parser found for '%s' (bufnr=%d)", bufname0, bufnr0)
+  if not utils.has_ts_parser(lang, "locals") then
+    utils.info("No treesitter parser or no 'locals.scm' found for '%s' (bufnr=%d)", bufname0, bufnr0)
     return
   end
 

lua/fzf-lua/utils.lua
@@ -7,6 +7,7 @@ function _G.dump(...)
 end
 
 local uv = vim.uv or vim.loop
+local memoize = vim.func and vim.func._memoize
 
 local M = {}
 
@@ -1467,17 +1468,20 @@ end
 
 --- Checks if treesitter parser for language is installed
 ---@param lang string
+---@param query_name string
 ---@return boolean
-function M.has_ts_parser(lang)
-  if M.__HAS_NVIM_011 then
-    return vim.treesitter.language.add(lang)
-        -- ensure the language has a highlights parser or we get
-        -- no highlights for langugaes like json/jsonc/toml/etc
-        and #vim.treesitter.query.get_files(lang, "highlights") > 0
-        or false
-  else
-    return (pcall(vim.treesitter.language.add, lang))
-  end
+function M.has_ts_parser(lang, query_name)
+  -- ensure the language has a highlights parser or we get
+  -- no highlights for langugaes like json/jsonc/toml/etc
+  return (M.__HAS_NVIM_011 and vim.treesitter.language.add(lang)
+        or (pcall(vim.treesitter.language.add, lang)))
+      and #vim.treesitter.query.get_files(lang, query_name) > 0 or false
+end
+
+-- query.get_files, which looks expensive recursively check modeline
+-- also call nvim_get_runtime_file -> shell scripts
+if memoize then
+  M.has_ts_parser = memoize("concat-2", M.has_ts_parser, false)
 end
 
 --- Wrapper around vim.lsp.jump_to_location which was deprecated in v0.11

lua/fzf-lua/win.lua
@@ -1029,7 +1029,7 @@ function FzfWin:treesitter_attach()
 
           local lang = vim.treesitter.language.get_lang(ft)
           if not lang then return end
-          local loaded = lang and utils.has_ts_parser(lang)
+          local loaded = lang and utils.has_ts_parser(lang, "highlights")
           if not loaded then return end
 
           -- NOTE: if the line contains unicode characters `#line > win_width`

commit 23fca9ac345b8b7da326f16d0d1db239f293b43c

fix(ts-context): regression from a953548 (#2366)

lua/fzf-lua/previewer/builtin.lua
@@ -1001,9 +1001,14 @@ function Previewer.base:update_ts_context()
     TSContext.close(self.win.preview_winid)
     return
   end
-  local parser, err = vim.treesitter.get_parser(self.preview_bufnr, lang)
+  local parser, err = vim.treesitter.get_parser(self.preview_bufnr, lang, { error = false })
+  -- TODO: fix `has_ts_parser` regression from a953548 (#2366)
   -- should never fail since `utils.has_ts_parser` returned true
-  assert(parser, "'vim.treesitter.get_parser' err: " .. tostring(err))
+  -- assert(parser, "'vim.treesitter.get_parser' err: " .. tostring(err))
+  if not parser or err then
+    TSContext.close(self.win.preview_winid)
+    return
+  end
   local context_updated
   TSContext.zindex = self.win.winopts.zindex + 20
   for _, t in ipairs({ 0, 20, 50, 100 }) do