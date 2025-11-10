ibhagwan/fzf-lua

commit d7497da366b653cd140082bcd8184d7dfa877a23

fix: restore mode on hide from terminal mode (#2054)

lua/fzf-lua/actions.lua
@@ -129,10 +129,6 @@ end
 
 -- Dummy abort action for `esc|ctrl-c|ctrl-q`
 M.dummy_abort = function(_, o)
-  -- try to resume mode if `complete` is set
-  if o.complete and o.__CTX.mode == "i" then
-    vim.cmd [[noautocmd lua vim.api.nvim_feedkeys('i', 'n', true)]]
-  end
 end
 
 M.resume = function(_, _)

lua/fzf-lua/win.lua
@@ -1312,6 +1312,12 @@ function FzfWin:close(fzf_bufnr, hide, hidden)
     vim.o.hlsearch = true
     self.hls_on_close = nil
   end
+  -- Restore insert/normal-terminal mode (#2054)
+  if utils.__CTX().mode == "nt" then
+    utils.feed_keys_termcodes([[<C-\><C-n>]])
+  elseif utils.__CTX().mode == "i" then
+    vim.cmd [[noautocmd lua vim.api.nvim_feedkeys('i', 'n', true)]]
+  end
   if self.winopts and type(self.winopts.on_close) == "function" then
     self.winopts.on_close()
   end

commit b19dabe6ddc0b1cef4e0b30e864aca88fdabc551

fix: regression from d7497da (closes #2417)

lua/fzf-lua/actions.lua
@@ -129,6 +129,10 @@ end
 
 -- Dummy abort action for `esc|ctrl-c|ctrl-q`
 M.dummy_abort = function(_, o)
+  -- try to resume mode if `complete` is set
+  if o.complete and o.__CTX.mode == "i" then
+    vim.cmd [[noautocmd lua vim.api.nvim_feedkeys('i', 'n', true)]]
+  end
 end
 
 M.resume = function(_, _)

lua/fzf-lua/win.lua
@@ -1318,8 +1318,6 @@ function FzfWin:close(fzf_bufnr, hide, hidden)
   -- Restore insert/normal-terminal mode (#2054)
   if utils.__CTX().mode == "nt" then
     utils.feed_keys_termcodes([[<C-\><C-n>]])
-  elseif utils.__CTX().mode == "i" then
-    vim.cmd [[noautocmd lua vim.api.nvim_feedkeys('i', 'n', true)]]
   end
   if self.winopts and type(self.winopts.on_close) == "function" then
     self.winopts.on_close()