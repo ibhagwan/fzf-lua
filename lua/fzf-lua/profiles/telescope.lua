94e2ae01cbc76ba4aa61e2c7b38fe7f25159fc29
bhagwan <bhagwan@disroot.org>
2026-01-28 19:45:34 -0500

    fix(telescope): buffers/ctrl-d (closes #2539)

--- lua/fzf-lua/profiles/telescope.lua
+++ lua/fzf-lua/profiles/telescope.lua
@@ -82,7 +82,7 @@ return {
   },
   buffers    = {
     keymap = { builtin = { ["<C-d>"] = false } },
-    actions = { ["ctrl-x"] = false, ["ctrl-d"] = { actions.buf_del, actions.resume } },
+    actions = { ["ctrl-x"] = false, ["ctrl-d"] = { fn = actions.buf_del, reload = true } },
   },
   defaults   = { git_icons = false },
 }
