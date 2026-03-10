7dbebde3d57379b2da6fe9619050775adfd6b71e
Chandler Anderson <chanderson90@gmail.com>
2026-03-10 11:57:04 -0400

    fix: exclude .jj directory from file search defaults
    Jujutsu (jj) stores internal data in a .jj directory. Exclude it from
    the default fd_opts, rg_opts, and find_opts so .jj contents don't
    appear in file picker results.

--- lua/fzf-lua/defaults.lua
+++ lua/fzf-lua/defaults.lua
@@ -522,9 +522,9 @@ M.defaults.files  = {
   _fzf_nth_devicons      = true,
   git_status_cmd         = {
     "git", "-c", "color.status=false", "--no-optional-locks", "status", "--porcelain=v1" },
-  find_opts              = [[-type f \! -path '*/.git/*']],
-  rg_opts                = [[--color=never --files -g "!.git"]],
-  fd_opts                = [[--color=never --type f --type l --exclude .git]],
+  find_opts              = [[-type f \! -path '*/.git/*' \! -path '*/.jj/*']],
+  rg_opts                = [[--color=never --files -g "!.git" -g "!.jj"]],
+  fd_opts                = [[--color=never --type f --type l --exclude .git --exclude .jj]],
   dir_opts               = [[/s/b/a:-d]],
   hidden                 = true,
   toggle_ignore_flag     = "--no-ignore",
