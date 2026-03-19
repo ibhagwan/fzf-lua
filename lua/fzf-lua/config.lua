ibhagwan/fzf-lua

commit 1f3e985e137f10f3485481219161c7f70f354838

feaT(git): binds for commits <-> diff <-> hunks

lua/fzf-lua/config.lua
@@ -983,6 +983,12 @@ function M.normalize_opts(opts, globals, __resume_key) ---@diagnostic disable
     opts.fn_preprocess = [[return require("fzf-lua.make_entry").preprocess]]
   end
 
+  -- set by git_{commits|diff|hunks} actions
+  if opts["__pos_" .. utils.get_info().cmd] then
+    opts.locate = opts.locate == nil and true or opts.locate
+    opts.__locate_pos = opts.__locate_pos or opts["__pos_" .. utils.get_info().cmd]
+  end
+
   if opts.locate and utils.has(opts, "fzf", { 0, 36 }) then
     table.insert(opts._fzf_cli_args, "--bind=" .. libuv.shellescape("load:+transform:"
       .. FzfLua.shell.stringify_data(function(_, _, _)

lua/fzf-lua/defaults.lua
@@ -668,11 +668,8 @@ M.defaults.git                   = {
   ---@class fzf-lua.config.GitDiff: fzf-lua.config.GitBase
   ---Git reference(s) to compare against.
   ---@field ref? string|string[]
-  -- `compare_against` was renamed to `ref1`
   ---Git reference used as the base for the comparison.
   ---@field ref1? string
-  ---Fzf cursor start position for ctrl-q git_commits
-  ---@field _pos? number
   diff = {
     cmd               = "git --no-pager diff --name-only {ref1} {ref}",
     ref               = nil,
@@ -688,14 +685,22 @@ M.defaults.git                   = {
     _actions          = function() return M.globals.actions.files end,
     _headers          = { "cwd", "actions" },
     actions           = {
-      ["ctrl-q"] = {
-        fn = function(_, _o)
+      ["ctrl-d"] = {
+        fn = function(s, _o)
+          if not s[1] then return FzfLua.utils.fzf_exit() end
           local o = vim.deepcopy(_o.__call_opts)
-          if o._pos then
-            o._fzf_cli_args = o._fzf_cli_args or {}
-            table.insert(o._fzf_cli_args, "--bind="
-              .. FzfLua.libuv.shellescape(string.format("load:+pos(%d)", o._pos)))
-          end
+          o["__pos_" .. FzfLua.get_info().cmd] = tonumber(s[2])
+          o.file = FzfLua.path.entry_to_file(s[1], _o).path
+          FzfLua.git_hunks(o)
+        end,
+        header = "git hunks",
+        exec_silent = true,
+        field_index = "{} $FZF_POS",
+      },
+      ["ctrl-q"] = {
+        fn = function(s, _o)
+          local o = vim.deepcopy(_o.__call_opts)
+          o["__pos_" .. FzfLua.get_info().cmd] = tonumber(s[2])
           FzfLua.git_commits(o)
         end,
         reuse = true,
@@ -705,12 +710,17 @@ M.defaults.git                   = {
   },
   ---Git diff hunks (changed lines).
   ---@class fzf-lua.config.GitHunks: fzf-lua.config.GitBase
-  ---Git reference to compare against.
-  ---@field ref? string
+  ---Git file to diff
+  ---@field file? string
+  ---Git reference(s) to compare against.
+  ---@field ref? string|string[]
+  ---Git reference used as the base for the comparison.
+  ---@field ref1? string
   hunks = {
     previewer         = M._default_previewer_fn,
-    cmd               = "git --no-pager diff --color=always {ref}",
-    ref               = "HEAD",
+    cmd               = "git --no-pager diff --color=always {ref1} {ref} {file}",
+    ref               = nil,
+    ref1              = nil,
     multiprocess      = true, ---@type integer|boolean
     fn_transform      = [[return require("fzf-lua.make_entry").git_hunk]],
     fn_preprocess     = [[return require("fzf-lua.make_entry").preprocess]],
@@ -723,7 +733,14 @@ M.defaults.git                   = {
     },
     _fzf_nth_devicons = true,
     _actions          = function() return M.globals.actions.files end,
-    _headers          = { "cwd" },
+    _headers          = { "cwd", "actions" },
+    actions           = {
+      ["ctrl-q"] = {
+        fn = function(_, _o) FzfLua.git_diff(vim.deepcopy(_o.__call_opts)) end,
+        reuse = true,
+        header = "git diff"
+      },
+    },
   },
   ---Git commits (project).
   ---@class fzf-lua.config.GitCommits: fzf-lua.config.GitBase
@@ -741,7 +758,7 @@ M.defaults.git                   = {
           local o = vim.deepcopy(_o.__call_opts)
           o.ref = s[1]:match("[^ ]+")
           o.ref1 = o.ref .. "~"
-          o._pos = tonumber(s[2])
+          o["__pos_" .. FzfLua.get_info().cmd] = tonumber(s[2])
           FzfLua.git_diff(o)
         end,
         header = "git diff",

lua/fzf-lua/providers/git.lua
@@ -104,12 +104,9 @@ local git_validate_ref = function(opts, ref)
   return true
 end
 
----@param opts fzf-lua.config.GitDiff|{}?
----@return thread?, string?, table?
-M.diff = function(opts)
-  ---@type fzf-lua.config.GitDiff
-  opts = config.normalize_opts(opts, "git.diff")
-  if not opts then return end
+---@param opts fzf-lua.config.GitDiff|fzf-lua.config.GitHunks
+---@return table?
+local normalize_diff_opts = function(opts)
   -- Backward compat `compare_against` -> `ref1`
   ---@diagnostic disable-next-line: undefined-field
   opts.ref1 = opts.compare_against or opts.ref1
@@ -137,15 +134,44 @@ M.diff = function(opts)
   end
   opts.cmd = opts.cmd:gsub("[<{]ref[}>]", opts.ref or "")
   opts.cmd = opts.cmd:gsub("[<{]ref1[}>]", opts.ref1 or "")
-  opts.preview = opts.preview:gsub("[<{]ref[}>]", opts.ref or "")
-  opts.preview = opts.preview:gsub("[<{]ref1[}>]", opts.ref1 or "")
-  opts = set_git_cwd_args(opts)
-  if not opts.cwd then return end
-  opts.preview = git_preview(opts, "{-1}")
+  opts.cmd = opts.cmd:gsub("[<{]file[}>]", opts.file and libuv.shellescape(opts.file) or "")
+  if type(opts.preview) == "string" then
+    opts.preview = opts.preview:gsub("[<{]ref[}>]", opts.ref or "")
+    opts.preview = opts.preview:gsub("[<{]ref1[}>]", opts.ref1 or "")
+    opts.preview = git_preview(opts, "{-1}")
+  end
   if type(opts._headers) == "table" then
     table.insert(opts._headers, "ref")
     table.insert(opts._headers, "ref1")
   end
+  return set_git_cwd_args(opts)
+end
+
+---@param opts fzf-lua.config.GitDiff|{}?
+---@return thread?, string?, table?
+M.diff = function(opts)
+  ---@type fzf-lua.config.GitDiff
+  opts = config.normalize_opts(opts, "git.diff")
+  if not opts then return end
+  opts = normalize_diff_opts(opts)
+  if not opts or not opts.cwd then return end
+  return core.fzf_exec(opts.cmd, opts)
+end
+
+
+---@param opts fzf-lua.config.GitHunks|{}?
+---@return thread?, string?, table?
+M.hunks = function(opts)
+  ---@type fzf-lua.config.GitHunks
+  opts = config.normalize_opts(opts, "git.hunks")
+  if not opts then return end
+  opts = normalize_diff_opts(opts)
+  if not opts or not opts.cwd then return end
+
+  -- we don't need git icons since we get them
+  -- as part of our `git status -s`
+  opts.git_icons = false
+
   return core.fzf_exec(opts.cmd, opts)
 end
 
@@ -313,29 +339,6 @@ M.stash = function(opts)
     end
     return (not stash or not rest) and x or stash .. rest
   end
-
-  return core.fzf_exec(opts.cmd, opts)
-end
-
----@param opts fzf-lua.config.GitHunks|{}?
----@return thread?, string?, table?
-M.hunks = function(opts)
-  ---@type fzf-lua.config.GitHunks
-  opts = config.normalize_opts(opts, "git.hunks")
-  if not opts then return end
-  if type(opts.ref) == "string" and #opts.ref > 0 and not git_validate_ref(opts, opts.ref) then
-    return
-  end
-  opts.cmd = opts.cmd:gsub("[<{]ref[}>]", opts.ref or "")
-  opts = set_git_cwd_args(opts)
-  if not opts.cwd then return end
-
-  -- we don't need git icons since we get them
-  -- as part of our `git status -s`
-  opts.git_icons = false
-
-  opts.header_prefix = opts.header_prefix or "+ -  "
-  opts.header_separator = opts.header_separator or "|"
 
   return core.fzf_exec(opts.cmd, opts)
 end

commit 612ee9b080cad889e9ebfe76b08848392f856414

fix: regression from 1f3e985 (fix #2624)

lua/fzf-lua/config.lua
@@ -984,7 +984,7 @@ function M.normalize_opts(opts, globals, __resume_key) ---@diagnostic disable
   end
 
   -- set by git_{commits|diff|hunks} actions
-  if opts["__pos_" .. utils.get_info().cmd] then
+  if utils.get_info().cmd and opts["__pos_" .. utils.get_info().cmd] then
     opts.locate = opts.locate == nil and true or opts.locate
     opts.__locate_pos = opts.__locate_pos or opts["__pos_" .. utils.get_info().cmd]
   end