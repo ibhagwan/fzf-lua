7972e2a424a717feb1eaca240973e96e400cb8ea
phanium <91544758+phanen@users.noreply.github.com>
2026-04-18 18:25:54 +0800

    fix: revert 97a5b92d, bd9ead92

--- lua/fzf-lua/fzf.lua
+++ lua/fzf-lua/fzf.lua
@@ -194,8 +194,8 @@ function M.raw_fzf(contents, fzf_cli_args, opts)
       utils.feed_keys_termcodes("i")
     else
       -- TODO(#2683):
-      utils.feed_keys_termcodes("i")
-      -- vim.cmd [[startinsert]]
+      -- utils.feed_keys_termcodes("i")
+      vim.cmd [[startinsert]]
     end
   end
 
