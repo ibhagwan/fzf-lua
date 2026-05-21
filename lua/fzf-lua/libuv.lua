71b45a044c4ef6ecb1910880ef642d5a0e1c02e9
phanium <91544758+phanen@users.noreply.github.com>
2026-05-21 10:55:26 +0800

    fix: correctly vim.schedule the exit

--- lua/fzf-lua/libuv.lua
+++ lua/fzf-lua/libuv.lua
@@ -547,9 +547,9 @@ M.spawn_stdio = function(opts)
   local on_finish = function(code)
     pipe_close(stdout)
     pipe_close(stderr)
-    if vim.in_fast_event() and fn_postprocess then
+    if vim.in_fast_event() then
       vim.schedule(function()
-        fn_postprocess(opts)
+        if fn_postprocess then fn_postprocess(opts) end
         exit(code)
       end)
     else
